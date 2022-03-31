cef07eeceac7edf015b500cd7853601bc5acc93c
Eduardo <eduardo.carneiro2907@gmail.com>
2022-03-31 16:45:42 -0300

    Add paint function
    This function will let the user drawn on screen, by click and arresting
    the mouse, pixel-by-pixel. For now, it just changes the color from white to black.

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ for(i = 0; i < 256; i++) {
     squareList[i] = document.createElement('div');
     squareList[i].classList.add('square');
     squareList[i].classList.add('num' + i);
+    squareList[i].addEventListener('mouseover', paint)
+    squareList[i].addEventListener('click', paint)
     screen.appendChild(squareList[i]);
 }
+
+function paint(event) {
+    console.log(event)
+    if(event.buttons == 1 || event.type == 'click') {
+       event.target.style.backgroundColor = 'black' 
+    }
+}
+
