5c0bb2770895922e49358672617d373fb8952565
Eduardo <eduardo.carneiro2907@gmail.com>
2022-04-01 15:11:14 -0300

    Add color buttons

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-let screen = document.getElementById('screen')
-let squareList = []
+var color = 'black'
+
+let screen = document.getElementById('screen');
+let squareList = [];
 for(i = 0; i < 256; i++) {
     squareList[i] = document.createElement('div');
     squareList[i].classList.add('square');
     squareList[i].classList.add('num' + i);
-    squareList[i].addEventListener('mouseover', paint)
-    squareList[i].addEventListener('click', paint)
+    squareList[i].addEventListener('mouseover', paint);
+    squareList[i].addEventListener('mousedown', paint);
     screen.appendChild(squareList[i]);
 }
 
-function paint(event) {
-    console.log(event)
-    if(event.buttons == 1 || event.type == 'click') {
-       event.target.style.backgroundColor = 'black' 
+
+let buttonList = {
+    blackColor: document.getElementById('black'),
+    whiteColor: document.getElementById('white'),
+    rgbColor: document.getElementById('rgb_color')
+}
+    buttonList.blackColor.addEventListener('click', changeColor)
+    buttonList.whiteColor.addEventListener('click', changeColor)
+    buttonList.rgbColor.addEventListener('input', changeColor)
+
+function changeColor(event) {
+    if(event.type == 'click') {
+        color = event.target.id
+    }
+    else {
+        color = event.target.value
     }
 }
 
+function paint(event) {
+    if(event.buttons == 1 || event.type == 'mousedown') {
+       event.target.style.backgroundColor = color;
+    }
+}
