9fcbfcd067aab62afbb857e0289f1eb1044f832e
Eduardo <eduardo.carneiro2907@gmail.com>
2022-04-02 11:39:58 -0300

    Add clear button and grid resizer
    Also fixed a bug where grid could be selected by cursor.

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-var color = 'black'
-
-let screen = document.getElementById('screen');
-let squareList = [];
-for(i = 0; i < 256; i++) {
-    squareList[i] = document.createElement('div');
-    squareList[i].classList.add('square');
-    squareList[i].classList.add('num' + i);
-    squareList[i].addEventListener('mouseover', paint);
-    squareList[i].addEventListener('mousedown', paint);
-    screen.appendChild(squareList[i]);
+let gridSize = 256
+let pixelList = [];
+let pixelSize = 40
+const screen = document.getElementById('screen');
+function createGrid() {
+    for (i = 0; i < gridSize; i++) {
+        pixelList[i] = document.createElement('div');
+        pixelList[i].classList.add('square');
+        pixelList[i].classList.add('num' + i);
+        pixelList[i].addEventListener('mouseover', paint);
+        pixelList[i].addEventListener('mousedown', paint);
+        pixelList[i].style.width = `${pixelSize}px`
+        pixelList[i].style.height = `${pixelSize}px`
+        screen.appendChild(pixelList[i]);
+    }
 }
-
+createGrid(gridSize)
+function paint(event) {
+    if (event.buttons == 1 || event.type == 'mousedown') {
+        event.target.style.backgroundColor = color;
+    }
+}
 
 let buttonList = {
     blackColor: document.getElementById('black'),
     whiteColor: document.getElementById('white'),
     rgbColor: document.getElementById('rgb_color')
 }
-    buttonList.blackColor.addEventListener('click', changeColor)
-    buttonList.whiteColor.addEventListener('click', changeColor)
-    buttonList.rgbColor.addEventListener('input', changeColor)
-
+buttonList.blackColor.addEventListener('click', changeColor)
+buttonList.whiteColor.addEventListener('click', changeColor)
+buttonList.rgbColor.addEventListener('input', changeColor)
+buttonList.rgbColor.addEventListener('change', changeColor)
+let color = 'black'
 function changeColor(event) {
-    if(event.type == 'click') {
+    if (event.type == 'click') {
         color = event.target.id
     }
     else {
@@ -30,8 +40,18 @@ function changeColor(event) {
     }
 }
 
-function paint(event) {
-    if(event.buttons == 1 || event.type == 'mousedown') {
-       event.target.style.backgroundColor = color;
+function clean() {
+    for (i = 0; i < pixelList.length; i++) {
+        pixelList[i].style.backgroundColor = 'white'
     }
+}
+
+function resize(newSize) {
+    gridSize = newSize**2
+    for(i = 0; i < pixelList.length; i++) {
+        screen.removeChild(pixelList[i])
+    }
+    pixelList = []
+    pixelSize = (640*newSize)/gridSize
+    createGrid()
 }
